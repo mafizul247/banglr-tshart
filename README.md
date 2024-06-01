3c66dd3ebef823dd336338dc983256a847595295
mafizul247 <mafizul247@gmail.com>
2024-06-01 11:05:11 +0600

    Banglar T-Shart

---
+++ README.md
@@ -0,0 +1,33 @@
+# Banglar Tshart
+
+Banglar Tshart is a React-based web application where users can browse and purchase t-shirts. This project leverages several modern web development tools and practices, such as React Router DOM, conditional rendering, Context API, and React Hot Toast for notifications. The website is deployed on Netlify.
+
+## Live Website
+
+[Banglar Tshart](https://starlit-cannoli-9e8c60.netlify.app/)
+
+## Repository
+
+[GitHub Repository](https://github.com/mafizul247/banglr-tshart.git)
+
+## Features
+
+- **React Router DOM:** For routing and navigation within the application.
+- **Data Loading with Loader:** Efficient data fetching and loading.
+- **Conditional Rendering:** Utilizing ternary operators and conditional rendering for dynamic UI updates.
+- **Context API:** State management across the application.
+- **React Hot Toast:** For displaying notifications with customizable positions.
+- **Netlify Deployment:** Configured with redirects for SPA.
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js
+- npm or yarn
+
+### Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/mafizul247/banglr-tshart.git
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css';
+import toast from 'react-hot-toast';
+
+const Home = () => {
+    const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if (exists) {
+            toast.error('You have already added this t-shirt');
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+            toast.success('Your t-shart has been added');
+        }
+    }
+
+    const handleRemoveFromCart = id => {
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining);
+        toast.error('Your t-shart has been removed from cart');
+    }
+
+    return (
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+                {
+                    tshirts.map(tshirt => <TShirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></TShirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
